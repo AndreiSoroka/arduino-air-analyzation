986139d542ce4845aa1f32e9f671fb02da46bb6f
andreisoroka <unickpro@gmail.com>
2019-12-28 23:36:25 +0200

    refactoring and fix bugs

--- arduino_air_analyzation.ino
+++ arduino_air_analyzation.ino
@@ -23,7 +23,7 @@ int settingsMinGas = 15;
 
 /* Buttons */
 #define PIN_ANALOG_KEY A7
-unsigned int timestampButtonDown = 0;
+unsigned long timestampButtonDown = 0;
 boolean pressedButton = false;
 
 /* LCD Screen */
@@ -34,16 +34,6 @@ boolean pressedButton = false;
 #define LCD_D6 11
 #define LCD_D7 12
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-byte specialCharPercent0[8] = {
-        B00000,
-        B00000,
-        B00000,
-        B00000,
-        B00000,
-        B00000,
-        B00000,
-        B00000,
-};
 byte specialCharPercent1[8] = {
         B00000,
         B00000,
@@ -131,8 +121,6 @@ boolean isSettingsScreen = false;
 /* GAS */
 #define PIN_ANALOG_GAS A0 //подключение аналогового сигналоьного пина
 #define PIN_DIGITAL_GAS 8 //подключение цифрового сигнального пина
-#define GAS_AWARAGE_SIZE 10 // кол-во значений для создания среднего
-#define GAS_AWARAGE_TIME 1000 // интервал
 boolean panicMode = false;
 float gasValue = 0;
 float historyPerMin[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
@@ -140,30 +128,303 @@ float historyPer10Mins[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 float historyPerHour[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 float historyPer4Hours[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
-/**********
-* Setup
-*/
-void setup() {
-    pinMode(PIN_DIGITAL_GAS, INPUT);
-    Serial.begin(9600); //инициализация Serial порта
 
-    lcd.createChar(1, specialCharPercent0);
-    lcd.createChar(2, specialCharPercent1);
-    lcd.createChar(3, specialCharPercent2);
-    lcd.createChar(4, specialCharPercent3);
-    lcd.createChar(5, specialCharPercent4);
-    lcd.createChar(6, specialCharPercent5);
-    lcd.createChar(7, specialCharPercent6);
-    lcd.createChar(8, specialCharPercent7);
-    lcd.createChar(9, specialCharPercent7);
+/***************************************
+ ************** Helpers ****************
+ ***************************************/
 
-    lcd.begin(16, 2);
-    lcd.print("Soroka...");
-    delay(3333);
-    lcd.clear();
-    delay(777);
+int getSpecialCharForGas(float &value) {
+    return constrain(map((int) value, settingsMinGas, settingsMaxGas, 1, 9), 1, 9);
 }
 
+float getGasPercentValue() {
+    return analogRead(PIN_ANALOG_GAS) * 100.00 / 1024.00;
+}
+
+void saveDataToHistory(float value, float arrayLink[6]) {
+    for (int i = 5; i >= 1; --i) {
+        arrayLink[i] = arrayLink[i - 1];
+    }
+    arrayLink[0] = value;
+}
+
+float averageOfArray(float arrayLink[6]) {
+    float result = 0.0;
+    for (int i = 0; i < 6; ++i) {
+        result += arrayLink[i];
+    }
+    return result / 6;
+}
+
+int getScreenNumber(int screen) {
+    if (isSettingsScreen) {
+        if (screen > 3) {
+            return 1;
+        }
+        if (screen < 1) {
+            return 3;
+        }
+        return screen;
+    } else {
+        if (screen > 4) {
+            return 1;
+        }
+        if (screen < 1) {
+            return 4;
+        }
+        return screen;
+    }
+}
+
+void updateDataFromSensors(float &value, float arrayLink[6], const String &text) {
+    lcd.setCursor(0, 0);
+    lcd.write(getSpecialCharForGas(gasValue));
+    lcd.print(gasValue);
+    lcd.print("% ");
+
+    for (int i = 0; i < 6; ++i) {
+        int code = getSpecialCharForGas(arrayLink[i]);
+        if (code == 1) {
+            lcd.print(" ");
+        } else {
+            lcd.write(code);
+        }
+    }
+
+    lcd.print(text);
+}
+
+//void printArray(float arrayLink[6]){
+//  Serial.print("[");
+//  for (int i = 0; i < 6; ++i) {
+//    Serial.print(arrayLink[i]); Serial.print(",  ");
+//  }
+//  Serial.println("]");
+//}
+
+/////// Screens
+void Screen1() {
+    updateDataFromSensors(gasValue, historyPerMin, "6m");
+}
+
+void Screen2() {
+    updateDataFromSensors(gasValue, historyPer10Mins, "1h");
+}
+
+void Screen3() {
+    updateDataFromSensors(gasValue, historyPerHour, "6h");
+}
+
+void Screen4() {
+    updateDataFromSensors(gasValue, historyPer4Hours, "1d");
+}
+
+void ScreenSetting1() {
+    lcd.setCursor(0, 0);
+    lcd.write("Sound alarm:");
+    lcd.setCursor(0, 1);
+    lcd.write(settingsSound ? "on " : "off");
+}
+
+void ScreenSetting2() {
+    lcd.setCursor(0, 0);
+    lcd.write("Max gas percent:");
+    lcd.setCursor(0, 1);
+    lcd.write(settingsMaxGas);
+    lcd.write("%");
+}
+
+void ScreenSetting3() {
+    lcd.setCursor(0, 0);
+    lcd.write("Min gas percent:");
+    lcd.setCursor(0, 1);
+    lcd.write(settingsMinGas);
+    lcd.write("%");
+}
+
+void updateScreen(boolean isClear) {
+    if (isClear) {
+        lcd.clear();
+        Serial.println(isSettingsScreen);
+    }
+
+    if (isSettingsScreen) {
+        switch (currentScreen) {
+            case 1:
+                ScreenSetting1();
+                break;
+            case 2:
+                ScreenSetting2();
+                break;
+            case 3:
+                ScreenSetting3();
+                break;
+        }
+    } else {
+        switch (currentScreen) {
+            case 1:
+                Screen1();
+                break;
+            case 2:
+                Screen2();
+                break;
+            case 3:
+                Screen3();
+                break;
+            case 4:
+                Screen4();
+                break;
+        }
+    }
+}
+
+
+/////////// Actions
+void clickButtonHome() {
+    currentScreen = getScreenNumber(currentScreen + 1);
+    updateScreen(true);
+}
+
+void clickButtonUp() {
+    if (!isSettingsScreen) {
+        currentScreen = getScreenNumber(currentScreen + 1);
+        updateScreen(true);
+        return;
+    } else {
+        // Sound alarm
+        if (currentScreen == 1) {
+            settingsSound = !settingsSound;
+            updateScreen(false);
+        }
+    }
+}
+
+void clickButtonDown() {
+    if (!isSettingsScreen) {
+        currentScreen = getScreenNumber(currentScreen - 1);
+        updateScreen(true);
+        return;
+    } else {
+        // Sound alarm
+        if (currentScreen == 1) {
+            settingsSound = !settingsSound;
+            updateScreen(false);
+        }
+    }
+}
+
+void holdButton() {
+    isSettingsScreen = !isSettingsScreen;
+    currentScreen = 1;
+    updateScreen(true);
+}
+
+
+
+
+
+/***************************************
+ ************* Lifecycle ***************
+ ***************************************/
+
+/**
+ * Activity buttons
+ */
+void loopButtons() {
+    if (timestampButtonDown > millis()) {
+        return;
+    }
+
+    int keyValue = analogRead(PIN_ANALOG_KEY);
+    boolean keyPressed = keyValue > 400;
+
+    if (pressedButton == keyPressed) {
+        return;
+    }
+    pressedButton = keyPressed;
+
+    if (keyPressed) {
+        if (keyValue > 900) {
+            // button 1
+            clickButtonHome();
+        } else if (keyValue > 600 && keyValue < 750) {
+            // button 2
+            clickButtonUp();
+        } else if (keyValue > 450 && keyValue < 550) {
+            // button 3
+            clickButtonDown();
+        }
+    } else {
+        if (millis() - timestampButtonDown > 1000) {
+            holdButton();
+        }
+    }
+    timestampButtonDown = millis() + 222;
+}
+
+/**
+ * Loop per second
+ */
+void loopPerSecond(unsigned int currentValue, unsigned long &lastValue) {
+    if (currentValue == lastValue) {
+        return;
+    }
+    lastValue = currentValue;
+
+    gasValue = getGasPercentValue();
+    updateScreen(false);
+}
+
+/**
+ * Loop per 1 min
+ */
+void loopPerMin(unsigned int currentValue, unsigned long &lastValue) {
+    if (currentValue == lastValue) {
+        return;
+    }
+    lastValue = currentValue;
+
+    saveDataToHistory(getGasPercentValue(), historyPerMin);
+}
+
+/**
+ * Loop per 10 mins
+ */
+void loopPer10Mins(unsigned int currentValue, unsigned long &lastValue) {
+    if (currentValue == lastValue) {
+        return;
+    }
+    lastValue = currentValue;
+
+    saveDataToHistory(averageOfArray(historyPerMin), historyPer10Mins);
+}
+
+/**
+ * Loop per 1 hour
+ */
+void loopPerHour(unsigned int currentValue, unsigned long &lastValue) {
+    if (currentValue == lastValue) {
+        return;
+    }
+    lastValue = currentValue;
+
+    saveDataToHistory(averageOfArray(historyPer10Mins), historyPerHour);
+}
+
+
+/**
+ * Loop per 4 hour
+ */
+void loopPer4Hours(unsigned int currentValue, unsigned long &lastValue) {
+    if (currentValue == lastValue) {
+        return;
+    }
+    lastValue = currentValue;
+
+    saveDataToHistory(averageOfArray(historyPer10Mins), historyPer4Hours);
+}
+
+//////////////////////////////
 /**********
 * Loop
 */
@@ -187,268 +448,26 @@ void loop() {
     loopPer4Hours(millis() / TIME_4HOURS, last4Hours);
 }
 
-/**
- * Activity buttons
- */
-void loopButtons(){
-    if (timestampButtonDown>millis()){
-        return;
-    }
+/**********
+* Setup
+*/
+void setup() {
+    pinMode(PIN_DIGITAL_GAS, INPUT);
+    Serial.begin(9600); //инициализация Serial порта
 
-    int keyValue = analogRead(PIN_ANALOG_KEY);
-    boolean keyPressed = keyValue > 400;
+//    lcd.createChar(1, specialCharPercent0);
+    lcd.createChar(2, specialCharPercent1);
+    lcd.createChar(3, specialCharPercent2);
+    lcd.createChar(4, specialCharPercent3);
+    lcd.createChar(5, specialCharPercent4);
+    lcd.createChar(6, specialCharPercent5);
+    lcd.createChar(7, specialCharPercent6);
+    lcd.createChar(8, specialCharPercent7);
+    lcd.createChar(9, specialCharPercent8);
 
-    if (pressedButton == keyPressed){
-        return;
-    }
-    pressedButton = keyPressed;
-
-    if (keyPressed) {
-        if (keyValue > 900) {
-            // button 1
-            clickButtonHome();
-        }
-        else if (keyValue > 600 && keyValue < 750) {
-            // button 2
-            clickButtonUp();
-        }
-        else if (keyValue > 450 && keyValue < 550) {
-            // button 3
-            clickButtonDown();
-        }
-    } else {
-        if (millis() - timestampButtonDown > 1000) {
-            Serial.println(millis() - timestampButtonDown);
-            holdButton();
-        }
-    }
-    timestampButtonDown = millis() + 222;
-}
-
-/**
- * Loop per second
- */
-void loopPerSecond(unsigned int currentValue, unsigned long &lastValue) {
-    if (currentValue == lastValue) {
-        return;
-    }
-    lastValue = currentValue;
-
-    gasValue = getGasPercentValue();
-    updateScreen(false);
-}
-
-/**
- * Loop per 1 min
- */
-void loopPerMin(unsigned int currentValue, unsigned long &lastValue){
-    if (currentValue == lastValue) {
-        return;
-    }
-    lastValue = currentValue;
-
-    saveDataToHistory(getGasPercentValue(), historyPerMin);
-}
-
-/**
- * Loop per 10 mins
- */
-void loopPer10Mins(unsigned int currentValue, unsigned long &lastValue){
-    if (currentValue == lastValue) {
-        return;
-    }
-    lastValue = currentValue;
-
-    saveDataToHistory(avarageOfArray(historyPerMin), historyPer10Mins);
-}
-
-/**
- * Loop per 1 hour
- */
-void loopPerHour(unsigned int currentValue, unsigned long &lastValue){
-    if (currentValue == lastValue) {
-        return;
-    }
-    lastValue = currentValue;
-
-    saveDataToHistory(avarageOfArray(historyPer10Mins), historyPerHour);
-}
-
-
-
-/**
- * Loop per 4 hour
- */
-void loopPer4Hours(unsigned int currentValue, unsigned long &lastValue){
-    if (currentValue == lastValue) {
-        return;
-    }
-    lastValue = currentValue;
-
-    saveDataToHistory(avarageOfArray(historyPer10Mins), historyPer4Hours);
-}
-
-
-///////////////////////////////
-float getGasPercentValue(){
-    return analogRead(PIN_ANALOG_GAS) * 100.00 / 1024.00;
-}
-
-void saveDataToHistory(float value, float arrayLink[6]){
-    for (int i = 5; i >= 1; --i) {
-        arrayLink[i] = arrayLink[i - 1];
-    }
-    arrayLink[0] = value;
-}
-
-float avarageOfArray(float arrayLink[6]){
-    float result = 0.0;
-    for (int i = 0; i < 6; ++i) {
-        result += arrayLink[i];
-    }
-    return result/6;
-}
-
-//void printArray(float arrayLink[6]){
-//  Serial.print("[");
-//  for (int i = 0; i < 6; ++i) {
-//    Serial.print(arrayLink[i]); Serial.print(",  ");
-//  }
-//  Serial.println("]");
-//}
-
-/////// Screens
-void updateScreen(boolean isClear){
-    if (isClear){
-        lcd.clear();
-        Serial.println(isSettingsScreen);
-    }
-
-    if (isSettingsScreen){
-        switch(currentScreen) {
-            case 1: ScreenSetting1(); break;
-            case 2: ScreenSetting2(); break;
-            case 3: ScreenSetting3(); break;
-        }
-    } else {
-        switch(currentScreen) {
-            case 1: Screen1(); break;
-            case 2: Screen2(); break;
-            case 3: Screen3(); break;
-            case 4: Screen4(); break;
-        }
-    }
-}
-
-void Screen1(){
-    updateDataFromSensors(gasValue, historyPerMin, "6m");
-}
-void Screen2(){
-    updateDataFromSensors(gasValue, historyPer10Mins, "1h");
-}
-void Screen3(){
-    updateDataFromSensors(gasValue, historyPerHour, "6h");
-}
-void Screen4(){
-    updateDataFromSensors(gasValue, historyPer4Hours, "1d");
-}
-
-void ScreenSetting1(){
-    lcd.setCursor(0,0);
-    lcd.write("Sound alarm:");
-    lcd.setCursor(0,1);
-    lcd.write(settingsSound ? "on " : "off");
-}
-
-void ScreenSetting2(){
-    lcd.setCursor(0,0);
-    lcd.write("Max gas percent:");
-    lcd.setCursor(0,1);
-    lcd.write(settingsMaxGas);
-    lcd.write("%");
-}
-
-void ScreenSetting3(){
-    lcd.setCursor(0,0);
-    lcd.write("Min gas percent:");
-    lcd.setCursor(0,1);
-    lcd.write(settingsMinGas);
-    lcd.write("%");
-}
-
-
-void clickButtonHome(){
-    currentScreen = getScreen(currentScreen + 1);
-    updateScreen(true);
-}
-void clickButtonUp(){
-    if (!isSettingsScreen) {
-        currentScreen = getScreen(currentScreen + 1);
-        updateScreen(true);
-        return;
-    } else {
-        // Sound alarm
-        if (currentScreen == 1){
-            settingsSound = !settingsSound;
-            updateScreen(false);
-        }
-    }
-}
-void clickButtonDown(){
-    if (!isSettingsScreen) {
-        currentScreen = getScreen(currentScreen - 1);
-        updateScreen(true);
-        return;
-    } else {
-        // Sound alarm
-        if (currentScreen == 1){
-            settingsSound = !settingsSound;
-            updateScreen(false);
-        }
-    }
-}
-void holdButton(){
-    isSettingsScreen = !isSettingsScreen;
-    currentScreen = 1;
-    updateScreen(true);
-}
-
-int getScreen(int screen){
-    if (isSettingsScreen){
-        if (screen > 3){
-            return 1;
-        }
-        if (screen < 1) {
-            return 3;
-        }
-        return screen;
-    } else {
-        if (screen > 4){
-            return 1;
-        }
-        if (screen < 1) {
-            return 4;
-        }
-        return screen;
-    }
-}
-
-
-
-//////
-void updateDataFromSensors(float &value, float arrayLink[6], String text){
-    lcd.setCursor(0,0);
-    lcd.write(getSpecailCharForGas(gasValue));
-    lcd.print(gasValue);
-    lcd.print("% ");
-
-    for(int i = 0; i<6; ++i){
-        lcd.write(getSpecailCharForGas(arrayLink[i]));
-    }
-
-    lcd.print(text);
-}
-
-int getSpecailCharForGas(float &value) {
-    return constrain(map((int) value, settingsMinGas, settingsMaxGas, 1, 9), 1, 8);
+    lcd.begin(16, 2);
+    lcd.print("Soroka...");
+    delay(3333);
+    lcd.clear();
+    delay(777);
 }
